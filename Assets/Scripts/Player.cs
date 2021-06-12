3bd601536dd4c6e2185290f0468933f9487a81ca
Colin Sanders <ck.sanders4@gmail.com>
2021-06-11 23:47:16 -0500

    attempting to improve the player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,10 +7,13 @@ public class Player : MonoBehaviour
 
     public float moveSpeed = 5.0f;
 
+    private Rigidbody2D rigidBody;
+    private Vector2 velocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,26 +23,39 @@ public class Player : MonoBehaviour
         HandleMovement();
     }
 
+    private void FixedUpdate()
+    {
+        //rigidBody.MovePosition(rigidBody.position + velocity * Time.fixedDeltaTime);
+        //rigidBody.velocity = velocity;
+    }
+
     void HandleMovement()
     {
+        //Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        //velocity = input * moveSpeed;
+
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.up * moveSpeed * Time.deltaTime, Space.World);
+            //rigidBody.MovePosition(transform.position + (Vector3.up * moveSpeed) * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(Vector3.down * moveSpeed * Time.deltaTime, Space.World);
+            //rigidBody.MovePosition(transform.position + (Vector3.down * moveSpeed) * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector3.left * moveSpeed * Time.deltaTime, Space.World);
+            //rigidBody.MovePosition(transform.position + (Vector3.left * moveSpeed) * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime, Space.World);
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime, Space.World); 
+            //rigidBody.MovePosition(transform.position + (Vector3.right * moveSpeed) * Time.deltaTime);
         }
     }
 
@@ -47,7 +63,7 @@ public class Player : MonoBehaviour
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         float angle = Mathf.Atan2(transform.position.y - mousePosition.y, transform.position.x - mousePosition.x) * Mathf.Rad2Deg;
-        print("Mouse position: " + mousePosition + ", angle: " + angle);
+        //print("Mouse position: " + mousePosition + ", angle: " + angle);
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle + 90f));
     }
 }
